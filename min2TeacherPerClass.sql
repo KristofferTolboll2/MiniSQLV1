5b9fe780e30c47df3660382e908d8353442a041e
Jonas Grønbek <32902073+JonasGroenbek@users.noreply.github.com>
2020-02-25 22:09:51 +0100

    Create min2TeacherPerClass.sql

---
+++ min2TeacherPerClass.sql
@@ -0,0 +1,30 @@
+DROP TRIGGER teacherTeamSizeCheckOnClassInsert ON classes;
+DROP FUNCTION teacherTeamSizeCheck; --trigger is dependent on function
+
+CREATE FUNCTION teacherTeamSizeCheck()
+  RETURNS trigger AS
+$func$
+BEGIN
+   	IF (
+        /*
+		(SELECT amount FROM 
+		(SELECT COUNT(teacherid) AS amount
+        FROM classes 
+        JOIN teacher_teacherteam 
+        ON classes.teacherteamid = teacher_teacherteam.teacherteamid
+        WHERE teacher_teacherteam.teacherteamid = NEW.teacherteamid
+		*/
+		(SELECT COUNT(teacherid) AS amount
+        FROM teacher_teacherteam 
+        WHERE teacher_teacherteam.teacherteamid = NEW.teacherteamid)
+	    < 2 )
+	THEN
+      RAISE EXCEPTION 'Not sufficient teachers on the teacherteam';
+   	END IF;
+   RETURN NEW;
+END
+$func$  
+LANGUAGE plpgsql;
+
+CREATE TRIGGER teacherTeamSizeCheckOnClassInsert BEFORE INSERT ON classes
+	FOR EACH ROW EXECUTE PROCEDURE teacherteamsizecheck();
