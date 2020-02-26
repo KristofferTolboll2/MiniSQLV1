c9108e393566357927a4797ba711c87b44f8b8e0
Jonas Grønbek <32902073+JonasGroenbek@users.noreply.github.com>
2020-02-26 13:16:26 +0100

    Create max2ActiveTeacherTeamsPerTeacher.sql

---
+++ max2ActiveTeacherTeamsPerTeacher.sql
@@ -0,0 +1,51 @@
+DROP TRIGGER IF EXISTS isTeacherAvailable ON teacher_teacherteam;
+DROP FUNCTION IF EXISTS isTeacherAvailaibleTeacher_teacherteam; --trigger is dependent on function
+
+CREATE FUNCTION isTeacherAvailaibleTeacher_teacherteam()
+  RETURNS trigger AS
+$func$
+BEGIN
+   	IF (
+		(SELECT count(*) as amount
+		FROM classes 
+		JOIN teacher_teacherteam 
+		ON (classes.teacherteamid = teacher_teacherteam.teacherteamid)
+		WHERE teacher_teacherteam.teacherid = NEW.teacherid AND classes.ends > now())
+		>= 2 )
+	THEN 
+		RAISE EXCEPTION 'Teacher is currently assigned two active teacherteams';
+   	END IF;
+   RETURN NEW;
+END
+$func$  
+LANGUAGE plpgsql;
+
+CREATE TRIGGER isTeacherAvailable BEFORE INSERT ON teacher_teacherteam
+	FOR EACH ROW EXECUTE PROCEDURE isTeacherAvailaibleTeacher_teacherteam();
+
+----------------------------------------------------------------------------------------------
+
+DROP TRIGGER IF EXISTS isTeacherAvailable ON classes;
+DROP FUNCTION IF EXISTS isTeacherAvailaibleClasses; --trigger is dependent on function
+
+CREATE FUNCTION isTeacherAvailaibleClasses()
+  RETURNS trigger AS
+$func$
+BEGIN
+   	IF (
+		(SELECT count(*) as amount
+		FROM classes 
+		JOIN teacher_teacherteam 
+		ON (classes.teacherteamid = teacher_teacherteam.teacherteamid)
+		WHERE teacher_teacherteam.teacherteamid = NEW.teacherteamid AND classes.ends > now())
+		>= 2 )
+	THEN
+            RAISE EXCEPTION 'Teacher is currently assigned two active teacherteams';
+   	END IF;
+   RETURN NEW;
+END
+$func$  
+LANGUAGE plpgsql;
+
+CREATE TRIGGER isTeacherAvailable BEFORE INSERT ON classes
+	FOR EACH ROW EXECUTE PROCEDURE isTeacherAvailaibleClasses();
