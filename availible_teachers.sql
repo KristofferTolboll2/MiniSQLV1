e5426fe21a6460707ed00525374bad290309719c
KristofferTolboll2 <43782552+KristofferTolboll2@users.noreply.github.com>
2020-02-26 22:47:46 +0100

    Create availible_teachers.sql
    orale

---
+++ availible_teachers.sql
@@ -0,0 +1,13 @@
+ CREATE OR REPLACE FUNCTION find_availible_teachers ()
+	RETURNS TABLE (teacherid INT, firstname text, lastname text) 
+AS $func$
+BEGIN
+RETURN QUERY
+SELECT DISTINCT t.teacherid, t.firstname, t.lastname
+FROM classes c
+JOIN teacher_teacherteam tt ON tt.teacherteamid = c.teacherteamid
+JOIN teachers t ON t.teacherid = tt.teacherid
+--WHERE c.starts > NOW();
+WHERE c.starts > '2000-01-01' AND c.ends < '2010-01-01';
+end; $func$
+LANGUAGE 'plpgsql';
